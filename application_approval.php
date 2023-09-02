2e7ceb228c550e914defc5d61fd27272bccfb50c
Taiseer517 <127662532+Taiseer517@users.noreply.github.com>
2023-09-02 20:12:01 +0600

    Add files via upload

--- application_approval.php
+++ application_approval.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" href="master.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" type="text/css" href="inbox.css">
     <title>Application Approval</title>
     <style>
         body {
@@ -21,6 +25,12 @@
     </style>
 </head>
 <body>
+
+<nav class="navbar navbar-inverse">
+    <div class="navbar-header">
+        <a class="navbar-brand" href="admin_dashboard.html">Rent IT</a>
+    </div>
+
     <h1>Application Approval</h1>
     <?php
     // Fetch application data from the database
